1eb00f155c447d257d38cc8a779c29b7f9b1607a
Inmeperial <quique_12_04@hotmail.com>
2023-10-16 20:37:04 -0300

    Added base for recursive function.
    Added base for recursive function and call it in the button function.

--- FrontEndUI/Scripts/LandingPage.js
+++ FrontEndUI/Scripts/LandingPage.js
@@ -1,13 +1,32 @@
 const URL = 'https://localhost:7055/api/NodeFiles';
+const maxForTreeRecu = 300;
 
-function displayItems(data) {
-  console.error(data);
+function recuMappingNodes(fatherNode, node, actualCount, maxCount) {
+  if (actualCount >= maxCount) {
+    return;
+  }
+  const liElement = document.createElement('li');
+  fatherNode.appendChild(liElement);
+
+  if (node.type === 'file') {
+    liElement.innerText = node.name;
+  } else {
+    const spanElement = document.createElement('span');
+    spanElement.innerText = node.name;
+    liElement.appendChild(spanElement);
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
-function GetNodes() {
+function GetNodes(indexVar) {
   fetch(URL)
     .then((response) => response.json())
-    .then((data) => displayItems(data))
+    .then((data) => {
+      const myUL = document.createElement('myUL');
+      indexVar.after(myUL);
+      for (let index = 0; index < data.length; index += 1) {
+        recuMappingNodes(myUL, data[index], 0, maxForTreeRecu);
+      }
+    })
     .catch((error) => console.error('Unable to get items.', error));
 }
