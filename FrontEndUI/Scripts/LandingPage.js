84f84b48dd38da2be00129222947fd0aefbc46da
Inmeperial <quique_12_04@hotmail.com>
2023-10-16 23:17:09 -0300

    Added new pages, icon and changed page for TreeView.

--- FrontEndUI/Scripts/LandingPage.js
+++ FrontEndUI/Scripts/LandingPage.js
@@ -1,51 +1,18 @@
-const URL = 'https://localhost:7055/api/NodeFiles';
-const maxForTreeRecu = 300;
-
-function addEventToElement(element) {
-  element.addEventListener('click', function (e) {
-    e.stopPropagation(); // needed to stop the propagation to others nodes.
-    this.querySelector('.nested').classList.toggle('active');
-    this.classList.toggle('caret-down');
-  });
-}
-
-function recuMappingNodes(fatherNode, node, actualCount, maxCount) {
-  if (actualCount >= maxCount) {
-    return;
-  }
-  const liElement = document.createElement('li');
-  fatherNode.appendChild(liElement);
-
-  if (node.type === 'file') {
-    liElement.innerText = node.name;
-  } else {
-    const spanElement = document.createElement('span');
-    spanElement.innerText = node.name;
-    liElement.appendChild(spanElement);
-    if (node.files != null && node.files.length > 0) {
-      spanElement.className = 'caret';
-      const ulElement = document.createElement('ul');
-      ulElement.className = 'nested';
-      spanElement.appendChild(ulElement);
-      addEventToElement(spanElement);
-
-      for (let index = 0; index < node.files.length; index += 1) {
-        recuMappingNodes(ulElement, node.files[index], actualCount + 1, maxCount);
-      }
-    }
-  }
-}
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
 // eslint-disable-next-line no-unused-vars
-function GetNodes(indexVar) {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((data) => {
-      const myUL = document.createElement('myUL');
-      indexVar.after(myUL);
-      for (let index = 0; index < data.length; index += 1) {
-        recuMappingNodes(myUL, data[index], 0, maxForTreeRecu);
-      }
-    })
-    .catch((error) => console.error('Unable to get items.', error));
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 }
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
+
+navLink.forEach((element) => element.addEventListener('click', closeMenu));
+hamburger.addEventListener('click', mobileMenu);
---
+++ FrontEndUI/Scripts/TreeViewPage.js
@@ -0,0 +1,51 @@
+const URL = 'https://localhost:7055/api/NodeFiles';
+const maxForTreeRecu = 300;
+
+function addEventToElement(element) {
+  element.addEventListener('click', function addEveToEl(e) {
+    e.stopPropagation(); // needed to stop the propagation to others nodes.
+    this.querySelector('.nested').classList.toggle('active');
+    this.classList.toggle('caret-down');
+  });
+}
+
+function recuMappingNodes(fatherNode, node, actualCount, maxCount) {
+  if (actualCount >= maxCount) {
+    return;
+  }
+  const liElement = document.createElement('li');
+  fatherNode.appendChild(liElement);
+
+  if (node.type === 'file') {
+    liElement.innerText = node.name;
+  } else {
+    const spanElement = document.createElement('span');
+    spanElement.innerText = node.name;
+    liElement.appendChild(spanElement);
+    if (node.files != null && node.files.length > 0) {
+      spanElement.className = 'caret';
+      const ulElement = document.createElement('ul');
+      ulElement.className = 'nested';
+      spanElement.appendChild(ulElement);
+      addEventToElement(spanElement);
+
+      for (let index = 0; index < node.files.length; index += 1) {
+        recuMappingNodes(ulElement, node.files[index], actualCount + 1, maxCount);
+      }
+    }
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function GetNodes(indexVar) {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const myUL = document.createElement('myUL');
+      indexVar.after(myUL);
+      for (let index = 0; index < data.length; index += 1) {
+        recuMappingNodes(myUL, data[index], 0, maxForTreeRecu);
+      }
+    })
+    .catch((error) => console.error('Unable to get items.', error));
+}
