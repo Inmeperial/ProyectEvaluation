a4457594dc327edc7cd5affb6d75e22352b42eff
Inmeperial <quique_12_04@hotmail.com>
2023-10-17 01:52:17 -0300

    Test for API added

---
+++ JsonAnalyzer_MService/JsonAnalyzer_MServiceTests/Controller/NodeFilesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JsonAnalyzer_MService.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonAnalyzer_MService.Controller.Tests
+{
+    [TestClass()]
+    public class NodeFilesControllerTests
+    {
+        [TestMethod()]
+        public void GetNodeFilesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
